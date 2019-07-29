5ae78f73888996c39f7a8f2e8068cfcb2fa90b28
Jian Li <jianl9@ics.uci.edu>
2019-07-28 23:44:09 -0700

    advacen features of lists and gesture

---
+++ src/pages/favorites/favorites.ts
@@ -0,0 +1,48 @@
+import { Component, OnInit, Inject } from '@angular/core';
+import { IonicPage, NavController, NavParams, ItemSliding } from 'ionic-angular';
+import { FavoriteProvider } from '../../providers/favorite/favorite';
+import { Dish } from '../../shared/dish';
+
+/**
+ * Generated class for the FavoritesPage page.
+ *
+ * See http://ionicframework.com/docs/components/#navigation for more info
+ * on Ionic pages and navigation.
+ */
+@IonicPage()
+@Component({
+  selector: 'page-favorites',
+  templateUrl: 'favorites.html',
+})
+export class FavoritesPage implements OnInit {
+
+  favorites: Dish[];
+  errMess: string;
+
+  constructor(public navCtrl: NavController, public navParams: NavParams,
+              private favoriteservice: FavoriteProvider,
+              @Inject('BaseURL') private BaseURL) {
+  }
+
+  ngOnInit() {
+    this.favoriteservice.getFavorites()
+      .subscribe(favorites => this.favorites = favorites,
+        errmess => this.errMess = errmess);
+  }
+
+  ionViewDidLoad() {
+    console.log('ionViewDidLoad FavoritesPage');
+  }
+
+  deleteFavorite(item: ItemSliding, id: number) {
+    // get id from favorite page and pass it to favorite service;
+    // deleteFavorite returns an observable so need to subscribe
+    console.log('delete', id);
+    this.favoriteservice.deleteFavorite(id)
+      .subscribe(favorites => this.favorites = favorites,
+        errmess => this.errMess = errmess);
+    item.close(); // hide the sliding out delete option btn when delete is done
+
+  }
+
+}
--- src/providers/favorite/favorite.ts
+++ src/providers/favorite/favorite.ts
@@ -1,6 +1,9 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import {Http} from "@angular/http";
+import { Http } from "@angular/http";
+import { Dish } from '../../shared/dish';
+import { Observable } from 'rxjs/Observable';
+import { DishProvider } from '../dish/dish';
 
 /*
   Generated class for the FavoriteProvider provider.
@@ -13,14 +16,49 @@ export class FavoriteProvider {
 
   favorites: Array<any>;
 
-  constructor(public http: Http) {
+  // inject dishservice into favorite to get access of dish service
+  constructor(public http: Http,
+              private dishservice: DishProvider) {
     console.log('Hello FavoriteProvider Provider');
     this.favorites = [];
   }
 
   addFavorite(id: number): boolean {
-    this.favorites.push(id);
+    // avoid duplicate
+    if (!this.isFavorite(id))
+      this.favorites.push(id);
+    console.log('favorites', this.favorites);
     return true;
+  }
+
+  getFavorites(): Observable<Dish[]> {
+    /* JS array method:
+    when I call that getDishes method, it'll return me all the dishes.
+    Now once I get all of the dishes I'm going to filter out only those dishes that are in my list of favorites.
+    Now how do I know if a particular dish is in my list of favorites?
+    Now this is where we saw that we already implemented this method here,
+    which allows me to check if a particular dish is in the list of favorites
+    So I'm going to use the same method here. So this is where I am using this favorites.some and then el,
+    and then the filter will be applied.
+    So only those dishes that are in my favorites will be selected.
+    And so the dishes array will contain only those dishes that
+    are in my favorites and that will be returned to my caller
+    when the getFavorites method will be called.
+     */
+    return this.dishservice.getDishes()
+      .map(dishes => dishes.filter(dish => this.favorites.some(el => el === dish.id)));
+  }
+
+  deleteFavorite(id: number): Observable<Dish[]> {
+    let index = this.favorites.indexOf(id);
+    if (index >= 0) {
+      this.favorites.splice(index,1);
+      return this.getFavorites(); // return modified array to the page
+    }
+    else {
+      console.log('Deleting non-existant favorite', id);
+      return Observable.throw('Deleting non-existant favorite' + id);
+    }
   }
 
   isFavorite(id: number): boolean {
