66e0a60c0dd22679f4ff6874b7f85f4c80d91bad
Jian Li <jianl9@ics.uci.edu>
2019-07-29 21:15:24 -0700

    Alerts, Toasts and Loading

--- src/pages/favorites/favorites.ts
+++ src/pages/favorites/favorites.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, Inject } from '@angular/core';
-import { IonicPage, NavController, NavParams, ItemSliding } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, ItemSliding, ToastController, LoadingController, AlertController } from 'ionic-angular';
 import { FavoriteProvider } from '../../providers/favorite/favorite';
 import { Dish } from '../../shared/dish';
 
@@ -21,7 +21,10 @@ export class FavoritesPage implements OnInit {
 
   constructor(public navCtrl: NavController, public navParams: NavParams,
               private favoriteservice: FavoriteProvider,
-              @Inject('BaseURL') private BaseURL) {
+              @Inject('BaseURL') private BaseURL,
+              public toastCtrl: ToastController,
+              private loadingCtrl: LoadingController,
+              private alertCtrl: AlertController) {
   }
 
   ngOnInit() {
@@ -37,10 +40,40 @@ export class FavoritesPage implements OnInit {
   deleteFavorite(item: ItemSliding, id: number) {
     // get id from favorite page and pass it to favorite service;
     // deleteFavorite returns an observable so need to subscribe
+    // pop up alert confirm msg
+    // create and present loading msg
+    // when server side done, close loading msg, present toast msg
     console.log('delete', id);
-    this.favoriteservice.deleteFavorite(id)
-      .subscribe(favorites => this.favorites = favorites,
-        errmess => this.errMess = errmess);
+    let alert = this.alertCtrl.create({
+      title: 'Confirm Delete',
+      message: 'Do you want to delete your Favorite Dish '+ id,
+      buttons: [
+        {
+          text: 'Cancel',
+          role: 'cancel',
+          handler: () => {
+            console.log('Delete cancelled');
+          }
+        },
+        {
+          text: 'Delete',
+          handler: () => {
+            let loading = this.loadingCtrl.create({
+              content: 'Deleting . . .'
+            });
+            let toast = this.toastCtrl.create({
+              message: 'Dish ' + id + ' deleted successfully',
+              duration: 3000});
+            loading.present();
+            this.favoriteservice.deleteFavorite(id)
+              .subscribe(favorites => {this.favorites = favorites; loading.dismiss(); toast.present(); } ,
+                errmess =>{ this.errMess = errmess; loading.dismiss(); });
+          }
+        }
+      ]
+    });
+
+    alert.present();
     item.close(); // hide the sliding out delete option btn when delete is done
 
   }
